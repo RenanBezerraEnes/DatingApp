52789109e6b17daeb06811f3324a57419168d75f
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-02-09 15:10:36 +0100

    Adding endpoint for get users and individual user

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,16 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 
 namespace API.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/[controller]")] //api//users
 public class UsersController : ControllerBase
 {
     private readonly DataContext _context;
     public UsersController(DataContext context)
     {
         _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.ToList();
+        return users;
+    }
+
+    [HttpGet("id")] //api/users/{id}
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        var user = _context.Users.Find(id);
+        return user;
+
+        //or return _context.Users.Find(id);
     }
 }
