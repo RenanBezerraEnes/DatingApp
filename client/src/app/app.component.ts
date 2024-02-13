1d37d1a43cf055104baa03ad21fbb8954873c48f
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-02-13 13:29:40 +0100

    Get method

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgIf } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, NgIf, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+  users$: Observable<any> | undefined;
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.users$ = this.http.get("https://localhost:7249/api/users")
+  }
 }
