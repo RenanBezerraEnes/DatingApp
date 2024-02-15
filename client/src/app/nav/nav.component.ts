5cd21428edf12257ae99183bd46a882c18ca6f5e
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-02-15 18:11:21 +0100

    Reactive Form, form control, form group...

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,13 +1,24 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-nav',
     standalone: true,
     imports: [
-        CommonModule,
+        CommonModule, ReactiveFormsModule
     ],
     templateUrl: './nav.component.html',
     styleUrls: ['./nav.component.css'],
 })
-export class NavComponent { }
+export class NavComponent { 
+    model:any = new FormGroup({
+        username: new FormControl(""),
+        password: new FormControl("")
+    })
+
+    login() {
+        console.log("Hello world", this.model);
+    }
+
+}
