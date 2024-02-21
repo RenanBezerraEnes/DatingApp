16da6719aeca45ddf7501a093ef6c0834b8af823
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-02-21 21:00:47 +0100

    Added auth guard for the routes + ng container

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -8,9 +8,15 @@ import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'members', component: MembersListComponent, canActivate: [authGuard]},
-    {path: 'members/:id', component: MemberDetailComponent},
-    {path: 'lists', component: ListsComponent},
-    {path: 'messages', component: MessagesComponent},
+    {path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children:[
+        {path: 'members', component: MembersListComponent},
+        {path: 'members/:id', component: MemberDetailComponent},
+        {path: 'lists', component: ListsComponent},
+        {path: 'messages', component: MessagesComponent},
+    ]
+    },
     {path: '**', component: HomeComponent, pathMatch: 'full'},
 ];
