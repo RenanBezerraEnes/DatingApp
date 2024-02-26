5ec4e95baee17e0f079c0d7d473662fbdca7c9b2
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-02-26 18:26:55 +0100

    member card and member detail

--- client/src/app/members/member-card/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card/member-card.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Member } from '../../../_models/member';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
   styleUrls: ['./member-card.component.css'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, RouterModule]
 })
 export class MemberCardComponent implements OnInit {
   @Input() member: Member | undefined;
--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, inject } from '@angular/core';
+import { Member } from '../../_models/member';
+import { MemberService } from '../../_services/member.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-detail',
@@ -8,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   imports: []
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member | undefined;
+  private memberService = inject(MemberService);
 
-  constructor() { }
+  constructor(private route: ActivatedRoute){}
 
   ngOnInit() {
+    this.loadMember();
+  }
+
+  loadMember(){
+    const username = this.route.snapshot.paramMap.get('username');
+    if(!username) return;
+
+    this.memberService.getMember(username).subscribe({
+      next: member => this.member = member
+    })
   }
 
 }
