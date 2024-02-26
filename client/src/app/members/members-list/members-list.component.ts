011c125fc3437ff10cebf7747710073137d7770e
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-02-26 15:09:11 +0100

    Displaying the members

--- client/src/app/members/members-list/members-list.component.ts
+++ client/src/app/members/members-list/members-list.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Member } from '../../_models/member';
+import { MemberService } from '../../_services/member.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-members-list',
   standalone: true,
   templateUrl: './members-list.component.html',
   styleUrls: ['./members-list.component.css'],
-  imports: []
+  imports: [CommonModule]
 })
 export class MembersListComponent implements OnInit {
-
+  members: Member[] = [];
+  private memberService = inject(MemberService);
   constructor() { }
 
   ngOnInit() {
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers().subscribe({
+      next: members => this.members = members
+    })
   }
 
 }
