db7ab7d1b63f42ba520b2ace086b59b89cce56ca
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-02-27 11:13:04 +0100

    I added some styles + ngx gallery

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,16 +3,20 @@ import { Member } from '../../_models/member';
 import { MemberService } from '../../_services/member.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+
 
 @Component({
   selector: 'app-member-detail',
   standalone: true,
   templateUrl: './member-detail.component.html',
   styleUrls: ['./member-detail.component.css'],
-  imports: [CommonModule]
+  imports: [CommonModule, TabsModule, GalleryModule]
 })
 export class MemberDetailComponent implements OnInit {
   member: Member | undefined;
+  images: GalleryItem[] = [];
   private memberService = inject(MemberService);
 
   constructor(private route: ActivatedRoute){}
@@ -26,8 +30,17 @@ export class MemberDetailComponent implements OnInit {
     if(!username) return;
 
     this.memberService.getMember(username).subscribe({
-      next: member => this.member = member
+      next: member => {this.member = member,
+      this.loadImages()
+      }
     })
+  }
+
+  loadImages(){
+    if(!this.member) return;
+    for(const photo of this.member?.photos){
+      this.images.push(new ImageItem({src: photo.url, thumb: photo.url}))
+    }
   }
 
 }
