cca1018b724f7ce819d8842269deec765ea3282e
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-02-29 09:28:18 +0100

    I am saving in memory my members, so I don't need to do the request again

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -2,25 +2,40 @@ import { Injectable, inject } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Member } from '../_models/member';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
   apiUrl = environment.baseUrl;
+  members: Member[] = [];
   private http = inject(HttpClient)
 
   getMembers(){
-    return this.http.get<Member[]>(this.apiUrl + 'users')
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.apiUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    )
   }
 
   getMember(username: string){
+    const member = this.members.find(x => x.userName == username);
+    if(member) return of(member);
+
     return this.http.get<Member>(this.apiUrl + 'users/' + username);
   }
 
   updateMember(member: Member){
-    console.log(member, 'Member');
-    return this.http.put(this.apiUrl + 'users', member);
+    return this.http.put(this.apiUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member}
+      })
+    )
   }
 
 }
