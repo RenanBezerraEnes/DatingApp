d49d21ad9e3f9d9d837b3627ee34803823facb67
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-03-02 15:03:23 +0100

    Reusable input component

---
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -0,0 +1,37 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.css'],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule]
+})
+export class TextInputComponent implements ControlValueAccessor {
+  @Input() label = '';
+  @Input() type = 'text';
+
+  constructor(@Self() public ngControl: NgControl) {
+    this.ngControl.valueAccessor = this;
+   }
+
+
+  writeValue(obj: any): void {
+    
+  }
+
+  registerOnChange(fn: any): void {
+    
+  }
+
+  registerOnTouched(fn: any): void {
+    
+  }
+
+  get control(): FormControl {
+    return this.ngControl.control as FormControl
+  }
+
+}
