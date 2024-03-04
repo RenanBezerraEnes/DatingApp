4d26a0c63549b9ba028027aa4245802ebc75ec99
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-03-04 09:40:53 +0100

    Default UserParams

---
+++ API/Helpers/UserParams.cs
@@ -0,0 +1,15 @@
+namespace API.Helpers
+{
+    public class UserParams
+    {
+        private const int MaxPageSize = 50;
+        public int PageNumber { get; set; } = 1;
+        private int _pagesSize = 10;
+        public int PageSize
+        {
+            get => _pagesSize;
+            set => _pagesSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+
+    }
+}
