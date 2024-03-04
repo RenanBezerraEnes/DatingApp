1003bb2e0f482648e2d22b666626493cfb277b43
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-03-04 09:42:05 +0100

    Add PagedList to receibe MemberDto as type

--- API/Interface/IUserRepository.cs
+++ API/Interface/IUserRepository.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 
 namespace API.interfaces
 {
@@ -11,7 +12,7 @@ namespace API.interfaces
         Task<AppUser> GetUserByIdAsync(int id);
         Task<AppUser> GetUserByUsernameAsync(string username);
 
-        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<PagedList<MemberDto>> GetMembersAsync(UserParams userParams);
 
         Task<MemberDto> GetMemberAsync(string username);
     }
