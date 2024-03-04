72d5716ea5e191ed74fbafe22f18ea4ce1f23242
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-03-04 10:34:24 +0100

    Add function to send parameters to getMembers from memberService

--- client/src/app/members/members-list/members-list.component.ts
+++ client/src/app/members/members-list/members-list.component.ts
@@ -4,20 +4,46 @@ import { MemberService } from '../../_services/member.service';
 import { CommonModule } from '@angular/common';
 import { MemberCardComponent } from "../member-card/member-card/member-card.component";
 import { Observable } from 'rxjs';
+import { Pagination } from '../../_models/pagination';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'app-members-list',
     standalone: true,
     templateUrl: './members-list.component.html',
     styleUrls: ['./members-list.component.css'],
-    imports: [CommonModule, MemberCardComponent]
+    imports: [CommonModule, MemberCardComponent, PaginationModule, FormsModule]
 })
 export class MembersListComponent implements OnInit {
-  members$: Observable<Member[]> | undefined;
+  // members$: Observable<Member[]> | undefined;
+  members: Member[] = [];
+  pagination: Pagination | undefined;
+  pageNumber = 1;
+  pageSize = 5;
+
   private memberService = inject(MemberService);
   constructor() { }
 
   ngOnInit() {
-    this.members$ = this.memberService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers(){
+    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: response => {
+        if(response.result && response.pagination){
+          this.members = response.result;
+          this.pagination = response.pagination;
+        }
+      }
+    })
+  }
+
+  pageChanged(event: any){
+    if(this.pageNumber !== event.page){
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 }
