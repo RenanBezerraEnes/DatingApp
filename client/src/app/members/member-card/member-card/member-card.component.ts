e7f0832252d85592f4e31cc05a8ce830f3387ddf
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-03-05 13:11:47 +0100

    Added the logic to like users

--- client/src/app/members/member-card/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card/member-card.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { Member } from '../../../_models/member';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MemberService } from '../../../_services/member.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-member-card',
@@ -13,9 +15,18 @@ import { RouterModule } from '@angular/router';
 export class MemberCardComponent implements OnInit {
   @Input() member: Member | undefined;
 
+  private memberService = inject(MemberService);
+  private toastr = inject(ToastrService);
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  addLike(member: Member){
+    this.memberService.addLike(member.userName).subscribe({
+      next: () => this.toastr.success("You've liked " + member.knownAs)
+    })
   }
 
 }
