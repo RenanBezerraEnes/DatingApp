6c5404c2a3e112bd7508340585c3e4fa49353b5f
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-03-06 14:53:17 +0100

    Message Entity + migration

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -29,9 +29,9 @@ public class AppUser
     public string City { get; set; }
 
     public string Country { get; set; }
-
     public List<Photo> Photos { get; set; } = new();
-
     public List<UserLike> LikedByUsers { get; set; }
     public List<UserLike> LikedUsers { get; set; }
+    public List<Message> MessageSent { get; set; }
+    public List<Message> MessageReceived { get; set; }
 }
