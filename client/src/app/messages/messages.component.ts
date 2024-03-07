3f4cca23e71f7a8b4a22483f5e2cfbc824e10c84
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-03-07 14:51:42 +0100

    Message component

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,17 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Message } from '../_models/message';
+import { Pagination } from '../_models/pagination';
+import { MessageService } from '../_services/message.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-messages',
   standalone: true,
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css'],
-  imports: []
+  imports: [CommonModule, FormsModule, PaginationModule, ButtonsModule, RouterModule ]
 })
 export class MessagesComponent implements OnInit {
+  messages?: Message[];
+  pagination?: Pagination;
+  container = 'Unread';
+  pageNumber = 1;
+  pageSize = 5;
 
-  constructor() { }
+  private messageService = inject(MessageService);
 
   ngOnInit() {
+    this.loadMessages();
+  }
+
+  loadMessages(){
+    this.messageService.getMessages(this.pageNumber, this.pageSize, this.container).subscribe({
+      next: response => {
+        this.messages = response.result;
+        this.pagination = response.pagination;
+      }
+    })
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
   }
 
 }
