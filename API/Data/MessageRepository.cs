9d58828c3265f2821c86afa75c238abee2fec750
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-03-07 16:35:07 +0100

    [fix]OrderBy

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -62,7 +62,7 @@ namespace API.Data
                     m.RecipientName == recipientName &&
                     m.SenderName == senderName
                 )
-                .OrderByDescending(m => m.MessageSent)
+                .OrderBy(m => m.MessageSent)
                 .ToListAsync();
 
             var unreadMessages = messages.Where(m => m.DateRead == null
