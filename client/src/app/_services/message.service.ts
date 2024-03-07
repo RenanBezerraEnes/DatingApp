689fe50e489d869f50e06dc43c9e4d13d18935a7
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-03-07 14:51:22 +0100

    Service for get messages/thread messages

---
+++ client/src/app/_services/message.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, inject } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { getPaginatedResult, getPaginationHeaders } from './paginationHelper.service';
+import { Message } from '../_models/message';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  apiUrl = environment.baseUrl;
+  private http = inject(HttpClient)
+
+  constructor() { }
+
+  getMessages(pageNumber: number, pageSize: number, container: string){
+    let params = getPaginationHeaders(pageNumber, pageSize);
+
+    params = params.append('Container', container);
+
+    return getPaginatedResult<Message[]>(this.apiUrl + 'messages', params, this.http);
+  }
+
+  getMessageThread(username: string){
+    return this.http.get<Message[]>(this.apiUrl + 'messages/thread/' + username);
+  }
+
+}
