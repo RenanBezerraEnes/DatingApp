9d41e0368d2959e7c98c205628aa40e31536ad74
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-03-11 12:55:12 +0100

    Adding admin role guard for admin route

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -38,6 +38,9 @@ login(model: any) {
  }
 
  setCurrentUser(user: User) {
+  user.roles = [];
+  const roles = this.getDecodedToken(user.token).role;
+  Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
   localStorage.setItem('user', JSON.stringify(user));
   this.currentUserSource.next(user);
  }
@@ -45,6 +48,10 @@ login(model: any) {
  logout(){
   localStorage.removeItem('user');
   this.currentUserSource.next(null);
+ }
+
+ getDecodedToken(token: string){
+  return JSON.parse(atob(token.split('.')[1]))
  }
 }
 
