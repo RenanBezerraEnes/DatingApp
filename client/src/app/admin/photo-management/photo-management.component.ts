add042ed8a662abd320e1aa8d832f90ffc0c79d3
RenanBezerraEnes <renanbezerraenes@icloud.com>
2024-03-11 17:06:25 +0100

    photo management

---
+++ client/src/app/admin/photo-management/photo-management.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-management',
+  templateUrl: './photo-management.component.html',
+  styleUrls: ['./photo-management.component.css'],
+  standalone: true,
+  imports: []
+})
+export class PhotoManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
